c28b9751e2254470a6314826ef43c7bf3403c43b
giovanni <gioarme1@gmail.com>
2024-07-30 06:08:00 +0200

    Aggiunti contatti

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,3 +1,13 @@
 @extends('Layout/layout')
 
 @section('title', 'Contact -')
+
+@section('container')
+    <hr>
+    <h1> E-mail : <a href="mailto:gioarme1@gmail.com">gioarme1@gmail.com</a></h1>
+    <hr>
+    <h1> Tel : +39 3394976856 </h1>
+    <hr>
+
+@endsection
+<link href="{{ asset('/css/contact.css') }}" rel="stylesheet">
